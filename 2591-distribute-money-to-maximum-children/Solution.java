a27edcbc739d2892e1ef1433727c9a37fb561472
SHRESTH KUMAR <206858669+ShresthChauhan431@users.noreply.github.com>
2026-07-27 11:26:31 +0530

    LeetSync: add solution for distribute-money-to-maximum-children (v1)

---
+++ 2591-distribute-money-to-maximum-children/Solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int distMoney(int money, int children) {
+        int curr = 0;
+        int count = 0;
+        while(money > 0){
+            int hi = money - 8;
+            if((hi == 4 || hi < children - curr + 1) && hi != 0){
+                break;
+            }
+            curr++;
+            money -= 8;
+        }
+        return curr;
+    }
+}
