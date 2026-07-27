f9054b0abe35d9c7e6560dba07c9893bdb03373b
SHRESTH KUMAR <206858669+ShresthChauhan431@users.noreply.github.com>
2026-07-27 11:56:32 +0530

    LeetSync: add solution for container-with-most-water (v2)

---
+++ 2591-container-with-most-water/Solution_v2.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int maxArea(int[] height) {
+        int i = 0; 
+        int j = height.length - 1;
+        int max = 0;
+        while (i < j) {
+            int h = Math.min(height[i], height[j]);
+            int w = j - i;
+            max = Math.max(max, h * w);
+            if (height[i] < height[j]) i++;
+            else if (height[i] > height[j]) {
+                i++;
+                j--;
+            }
+            else {
+                j--;
+            }
+        }
+        return max;
+    }
+}
