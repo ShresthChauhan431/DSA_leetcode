3f800f6b7d285e39dd9fdbfd85431fa6e78a3e18
SHRESTH KUMAR <206858669+ShresthChauhan431@users.noreply.github.com>
2026-08-07 16:59:00 +0530

    LeetSync: add solution for partition-array-into-two-arrays-to-minimize-sum-difference (v3)

---
+++ 2316-partition-array-into-two-arrays-to-minimize-sum-difference/Solution_v3.java
@@ -0,0 +1,70 @@
+class Solution {
+    void helper(int i, int end, int len, long sum, int[] nums, Map<Integer, Set<Long>> map){
+        if(i == end){
+            if(!map.containsKey(len)){
+                map.put(len, new HashSet<>());
+            }
+            map.get(len).add(sum);
+            return;
+        }
+        helper(i + 1, end, len + 1, sum + nums[i], nums, map);
+        helper(i + 1, end, len, sum, nums, map);
+    }
+    public int minimumDifference(int[] nums) {
+        Map<Integer, Set<Long>> map1 = new HashMap<>();
+        Map<Integer, Set<Long>> map2 = new HashMap<>();
+
+        int n = nums.length;
+        int mid = n/2;
+        long sum = 0;
+        for(int i: nums) sum += i;
+        helper(0, mid, 0, 0L, nums, map1);
+        helper(mid, n, 0, 0L, nums, map2);
+        Map<Integer, List<Long>> map = new HashMap<>();
+        for(Map.Entry<Integer, Set<Long>> entry: map2.entrySet()){
+            List<Long> l = new ArrayList<>(entry.getValue());
+            l.sort(null);
+            map.put(entry.getKey(), new ArrayList<>(l));
+        }
+
+        long ans = (long)1e9;
+
+        for(Map.Entry<Integer, Set<Long>> entry: map1.entrySet()) {
+
+            int needlen = mid - entry.getKey();
+            List<Long> list1 = new ArrayList<>(entry.getValue());
+
+            for(long a: list1) {
+                double b = sum / 2.0 - a;
+
+                List<Long> list2 = map.get(needlen);
+                int x = lowerBound(b, list2);
+
+                if(x < list2.size()) {
+                    long sum1 = a + list2.get(x);
+                    long sum2 = sum - sum1;
+                    ans = Math.min(ans, Math.abs(sum1 - sum2));
+                }
+                if(x > 0){
+                    long sum1 = a + list2.get(x - 1);
+                    long sum2 = sum - sum1;
+                    ans = Math.min(ans, Math.abs(sum1 - sum2));
+                }
+            }
+        }
+        return (int)ans;
+    }
+    public int lowerBound(double tar,  List<Long> list){
+        // list.sort(null);
+        int i = 0;
+        int j = list.size(); 
+        while(i < j){
+            int mid = i + (j - i)/2;
+            if(list.get(mid) < tar){
+                i = mid + 1;
+            }else
+                j = mid;
+        }
+        return i;
+    }
+}
