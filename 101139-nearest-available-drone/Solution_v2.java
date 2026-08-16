ce9157b2329946c3b2a49f17dd8cf2adf25f2a0d
SHRESTH KUMAR <206858669+ShresthChauhan431@users.noreply.github.com>
2026-08-16 08:09:26 +0530

    LeetSync: add solution for nearest-available-drone (v2)

---
+++ 101139-nearest-available-drone/Solution_v2.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int nearestDrone(int[][] drones, int[] target) {
+        int dist = (int)1e6; 
+        int ans = -1;
+        for(int i = 0; i < drones.length; i++){
+            int hi = Math.abs(drones[i][0] - target[0]) + Math.abs(drones[i][1] - target[1]);
+            if(hi <= drones[i][2] && hi < dist){
+                ans = i;
+                dist = hi;
+            }
+        }
+        return ans;
+    }
+}
