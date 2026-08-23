6f1ce463f9f051c4046c7aa8c1dc45b86ca45383
SHRESTH KUMAR <206858669+ShresthChauhan431@users.noreply.github.com>
2026-08-23 16:15:40 +0530

    LeetSync: add solution for check-ascii-palindromic (v2)

---
+++ 4030-check-ascii-palindromic/Solution_v2.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isPalindromic(String s) {
+        StringBuilder sb = new StringBuilder(); 
+        for(char c: s.toCharArray()){
+            int x = (int)c;
+            sb.append(String.format("%8s", Integer.toBinaryString(x)).replace(' ', '0'));
+        }
+        int i =0;
+        int j = sb.length() - 1;
+        // System.out.print(sb);
+        while(i < j){
+            if(sb.charAt(i) != sb.charAt(j)) return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
