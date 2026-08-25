6f286a223692713e31edf7cb3f5934491801130b
SHRESTH KUMAR <206858669+ShresthChauhan431@users.noreply.github.com>
2026-08-25 22:35:43 +0530

    LeetSync: add solution for smallest-missing-multiple-of-k (v1)

---
+++ 3718-smallest-missing-multiple-of-k/Solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int missingMultiple(int[] nums, int k) {
+        int ans = k; 
+        Set<Integer> set = new HashSet<>(); 
+        for(int i: nums)
+            set.add(i);
+        
+        for(int i = 1; i < nums.length + 2; i++){
+            if(!set.contains(k * i))
+                return k * i;
+        }
+        return ans + k;
+    }
+}
