10f0614d36aa7ea8ea4bdc9adfcdb5452f79080b
SHRESTH KUMAR <206858669+ShresthChauhan431@users.noreply.github.com>
2026-09-06 17:25:31 +0530

    LeetSync: add solution for count-good-cyclic-rotations (v1)

---
+++ 4043-count-good-cyclic-rotations/Solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int countGoodRotations(int[] nums) {
+        int halfSum = 0; 
+        int sum = 0; 
+        int n = nums.length;
+        for(int i = 0; i < n; i++){
+            if(i < n/2) halfSum += nums[i];
+            sum += nums[i];
+        }
+        int count = 0; 
+        if(halfSum > sum - halfSum) count++;
+        for(int i = 0; i < n; i++){
+            // int last =;
+            halfSum -= nums[i];
+            halfSum += nums[(n/2 + i) % n];
+            if(halfSum > sum - halfSum) count++;
+        }
+        return count;
+    }
+}
