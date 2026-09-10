9c2b6daa09871c79161ab7fad85dfa3d459ffdbb
SHRESTH KUMAR <206858669+ShresthChauhan431@users.noreply.github.com>
2026-09-11 00:18:40 +0530

    LeetSync: add solution for jump-game (v3)

---
+++ 55-jump-game/Solution_v3.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean helper(int i, int[] nums, int[] dp){
+        if(i >= nums.length - 1){
+            return true;
+        }
+        if(dp[i] != -1) return dp[i] == 1;
+        boolean ans = false;
+        for(int j = 1; j <= nums[i]; j++){
+            ans = helper(i + j, nums, dp);
+        }
+        if(ans){
+            dp[i] = 1;
+        }else{
+            dp[i] = 0;
+        }
+        return ans;
+    }
+    public boolean canJump(int[] nums) {
+        if(nums.length == 1) return true;
+        int[] dp = new int[nums.length];
+        Arrays.fill(dp, -1); 
+        return helper(0, nums, dp);
+    }
+}
