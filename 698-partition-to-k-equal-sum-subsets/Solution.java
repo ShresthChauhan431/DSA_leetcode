10240552feb91e02bc92c6b191938ccb13a7a38a
SHRESTH KUMAR <206858669+ShresthChauhan431@users.noreply.github.com>
2026-09-11 12:32:07 +0530

    LeetSync: add solution for partition-to-k-equal-sum-subsets (v1)

---
+++ 698-partition-to-k-equal-sum-subsets/Solution.java
@@ -0,0 +1,32 @@
+class Solution {
+    private void reverse(int[] a){
+        int l=0, r=a.length-1;
+        while(l<r){
+            int temp=a[l];
+            a[l]=a[r];
+            a[r]=temp;
+            l++; r--;
+        }
+    }
+    public boolean canPartitionKSubsets(int[] nums, int k) {
+        Arrays.sort(nums);
+        reverse(nums);
+        int sum = 0;
+        for(int i:nums) sum += i;
+        if(sum%k != 0)return false;
+        boolean[] used = new boolean[nums.length];
+        int mask = 0;
+        return helper(nums,0,k, sum/k, 0, mask);
+    }
+    public boolean helper(int[] nums,int i, int k,int tar,int curr,int mask){
+        if(k==1) return true;
+        if(curr == tar)return helper(nums,0,k-1,tar,0,mask);
+        for(int j=i; j<nums.length; j++){
+            if((mask & (1 << j)) == 0 && curr+nums[j]<=tar){
+                int nmask = mask | (1 << j);
+                if(helper(nums,j+1,k,tar,curr+nums[j],nmask))return true;
+            }
+        }
+        return false;
+    }
+}
