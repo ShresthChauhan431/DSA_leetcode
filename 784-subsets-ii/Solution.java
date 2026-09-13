2c2536c30d46ed0bb6a1151de8904e89d7e1b48a
SHRESTH KUMAR <206858669+ShresthChauhan431@users.noreply.github.com>
2026-09-13 19:36:04 +0530

    LeetSync: add solution for subsets-ii (v1)

---
+++ 784-subsets-ii/Solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        List<List<Integer>> list = new ArrayList<>(); 
+        Set<List<Integer>> set = new HashSet<>();
+        int n = nums.length; 
+        int total = 1 << n; 
+        for(int bitmask = 0; bitmask < total; bitmask++){
+            List<Integer> subset = new ArrayList<>();
+            for(int i = 0; i < nums.length; i++){
+                if((bitmask & (1 << i)) != 0){
+                    subset.add(nums[i]);
+                }
+            }
+            set.add(subset);
+        }
+        list.addAll(set);
+        return list;
+    }
+}
