0a037ff40fab5ac05c93524103f5339562602d52
SHRESTH KUMAR <206858669+ShresthChauhan431@users.noreply.github.com>
2026-09-13 14:33:11 +0530

    LeetSync: add solution for reverse-bits (v1)

---
+++ 136-reverse-bits/Solution.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int reverseBits(int n) {
+        int x = 0; 
+        for(int i = 0; i < 32; i++){
+            x = (x << 1) + (n & 1);
+            n = n >> 1;
+        }
+        return x;
+    }
+}
