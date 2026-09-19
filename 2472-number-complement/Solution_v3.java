eb945dd2a22c71e3ffb46931d76e228e303586f9
SHRESTH KUMAR <206858669+ShresthChauhan431@users.noreply.github.com>
2026-09-19 11:57:42 +0530

    LeetSync: add solution for number-complement (v3)

---
+++ 2472-number-complement/Solution_v3.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int findComplement(int num) {
+        int ans = 0;
+        int k = 0;
+        while(num > 0){
+            if((num & 1) == 0){
+                ans |= (1 << k);
+            }
+            k++;
+            num = num >> 1;
+        }
+        return ans;
+    }
+}
