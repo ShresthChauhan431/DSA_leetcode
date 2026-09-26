03652a0043ffc1af515e528f57986684080dcc8e
SHRESTH KUMAR <206858669+ShresthChauhan431@users.noreply.github.com>
2026-09-26 20:06:44 +0530

    LeetSync: add solution for minimum-queen-moves-to-reach-target (v1)

---
+++ 101181-minimum-queen-moves-to-reach-target/Solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int minQueenMoves(int[] source, int[] target) {
+        int sr = source[0], sc = source[1];
+        int tr = target[0], tc = target[1];
+
+        if(sr == tr && sc == tc) return 0;
+
+        if(sr == tr || sc == tc || Math.abs(sr - tr) == Math.abs(sc - tc)) {
+            return 1;
+        }
+        return 2;
+    }
+}
