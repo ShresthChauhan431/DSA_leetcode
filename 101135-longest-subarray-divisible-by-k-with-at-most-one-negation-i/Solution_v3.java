40bea09c20b6f00dd3b510a4b5cee38131f7c284
SHRESTH KUMAR <206858669+ShresthChauhan431@users.noreply.github.com>
2026-09-26 21:18:20 +0530

    LeetSync: add solution for longest-subarray-divisible-by-k-with-at-most-one-negation-i (v3)

---
+++ 101135-longest-subarray-divisible-by-k-with-at-most-one-negation-i/Solution_v3.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int longestSubarray(int[] nums, int k) {
+        int n = nums.length;
+        int[] prefix = new int[n + 1];
+        for(int i = 0; i < n; i++){
+            prefix[i + 1] = prefix[i] + nums[i];
+        }
+        int max = 0;
+        for(int i = 0; i < n; i++){
+            Set<Integer> set = new HashSet<>();
+            for(int j = i; j < n; j++){
+                int y = (prefix[j + 1] - prefix[i]) % k;
+                if(y < 0) y += k;
+                int x = (2 * nums[j]) % k;
+                if(x < 0) x += k;
+                set.add(x);
+                if(y == 0 || set.contains(y)){
+                    max = Math.max(max, j - i + 1);
+                }
+            }
+        }
+        return max;
+    }
+}
